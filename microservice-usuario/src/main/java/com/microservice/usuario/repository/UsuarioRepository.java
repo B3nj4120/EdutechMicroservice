675f71284f8374288da833f227384c86629105d9
benjamin <be.alegria@duocuc.cl>
2025-06-26 09:13:20 -0400

    servicefinish

--- microservice-usuario/src/main/java/com/microservice/usuario/repository/UsuarioRepository.java
+++ microservice-usuario/src/main/java/com/microservice/usuario/repository/UsuarioRepository.java
@@ -1,10 +1,22 @@
 package com.microservice.usuario.repository;
 
-import com.edutech.usuario.model.Usuario;
+import com.microservice.usuario.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.repository;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Optional<Usuario> findByUsername(String username);
+
+    //Buscar un usuario por su nombre de usuario
+    Optional<Usuario> findByusername(String username);
+
+    //buscar usuario por correo
+    Optional<Usuario> findBYEmail(String email);
+
+    //validar si ya existe un usuario con ese nombre
+    boolean existsByusername(String username);
+
+    //validar si ya existe un correo registrado
+    boolean existByEmail(String email);
 }
--- microservice-usuario/src/main/java/com/microservice/usuario/service/UsuarioService.java
+++ microservice-usuario/src/main/java/com/microservice/usuario/service/UsuarioService.java
@@ -17,24 +17,39 @@ public class UsuarioService {
      @Autowired
     private UsuarioRepository usuarioRepository;
 
+      // Obtener todos los usuarios
     public List<Usuario> findAll() {
         return usuarioRepository.findAll();
     }
 
+      // Buscar usuario por ID
     public Optional<Usuario> findById(int id) {
         return usuarioRepository.findById(id);
     }
 
+      // Guardar un nuevo usuario o actualizar uno existente
     public Usuario save(Usuario usuario) {
         return usuarioRepository.save(usuario);
     }
 
+     // Eliminar un usuario por ID
     public void delete(int id) {
         usuarioRepository.deleteById(id);
     }
 
+    // Verificar login (sin encriptación)
     public Optional<Usuario> login(String username, String password) {
         return usuarioRepository.findByUsername(username)
                 .filter(u -> u.getPassword().equals(password));
     }
+
+      // Validar si ya existe el username
+      public boolean existsByUsername(String username){
+        return usuarioRepository.existsByUsername(username);
+      }
+
+      // Validar si ya existe el email
+      public boolean existByEmail(String email){
+        return usuarioRepository.existByEmail(email);
+      }
 }
